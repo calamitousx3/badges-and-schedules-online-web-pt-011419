e85f46626cb714f77a37c5a1d499bf421600e845
Carmen Renee <carmengenpt@gmail.com>
2019-01-27 18:13:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name = "Arel")
+  "Hello, my name is #{name}."
+end 
+
+
+def batch_badge_creator(attendees)
+  attendees.map{|attendee| "Hello, my name is #{attendee}."}
+end
+
+def assign_rooms(attendees)
+  attendees.each_with_index.map {|attendee,index| "Hello, #{attendee}! You'll be assigned to room #{index+1}!"}
+end 
+
+def printer
+  
+end 
